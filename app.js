1e46242342c379e12306d58aabb3300f347b2aae
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-07 10:43:09 +0530

    shift mongodb to online server

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
 app.use(bodyParser.json());//parse the json data in the request body
 
 // import the router files
@@ -12,6 +15,7 @@ const menuRouter = require('./routes/menu');
 app.use('/person',personRouter);
 app.use('/menu',menuRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+app.listen(PORT, () => {
+    console.log('Server is running');
 });
--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 //it is responsible for establishing the connection between the server and the database.
 const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoURL = process.env.MONGODB_URL;
 
 // define the mongoDB connection string
-// const mongoURL = 'mongodb://localhost:27017/hotels';
-const mongoURL="mongodb+srv://msr:jai_uk@cluster0.hvfgsgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const mongoURL = process.env.MONGODB_URL_LOCAL;
 
 // set up the connection
 mongoose.connect(mongoURL);
