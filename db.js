f3595a0d3545265d8190ff9cf5e044b35e71b86a
Manoj Singh Rawat <natkhatkanharawatji@gmail.com>
2024-07-07 10:16:25 +0530

    changing mongoURL

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose');
 
 // define the mongoDB connection string
-const mongoURL = 'mongodb://localhost:27017/hotels';
+// const mongoURL = 'mongodb://localhost:27017/hotels';
+const mongoURL="mongodb+srv://msr:jai_uk@cluster0.hvfgsgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // set up the connection
 mongoose.connect(mongoURL);
